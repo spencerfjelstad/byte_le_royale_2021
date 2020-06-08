c15932c16027ae5a4083f72bfd2874a2e2fe9733
Sean Hagen <sean.r.hagen@gmail.com>
2020-06-08 11:22:23 -0500

    Made all changes that Wyly suggested

--- game/common/stats.py
+++ game/common/stats.py
@@ -1,21 +1,21 @@
-from game.common.enums import NodeType
+from game.common.enums import LocationType
 
 
 class GameStats:
-    node_reward_modifier = {
-        NodeType.grass_lands: .5,
-        NodeType.nord_dakotia: .6,
-        NodeType.mobave_desert: .7,
-        NodeType.mount_vroom: .8,
-        NodeType.loblantis: .8,
-        NodeType.tropical_cop_land: .9,
+    location_reward_modifier = {
+        LocationType.grass_lands: .5,
+        LocationType.nord_dakotia: .6,
+        LocationType.mobave_desert: .7,
+        LocationType.mount_vroom: .8,
+        LocationType.loblantis: .8,
+        LocationType.tropical_cop_land: .9,
     }
 
-    node_difficulty_modifier = {
-        NodeType.grass_lands: .5,
-        NodeType.nord_dakotia: .6,
-        NodeType.mobave_desert: .7,
-        NodeType.mount_vroom: .8,
-        NodeType.loblantis: .8,
-        NodeType.tropical_cop_land: .9,
+    location_difficulty_modifier = {
+        LocationType.grass_lands: .5,
+        LocationType.nord_dakotia: .6,
+        LocationType.mobave_desert: .7,
+        LocationType.mount_vroom: .8,
+        LocationType.loblantis: .8,
+        LocationType.tropical_cop_land: .9,
     }
