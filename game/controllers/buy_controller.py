6338207595e0c68de8a2b11edd5f91b5477daecf
[Sean Hagen] <sean.r.hagen@ndus.edu>
2020-10-03 13:07:16 -0500

    Buy controller?

--- game/controllers/buy_controller.py
+++ game/controllers/buy_controller.py
@@ -11,25 +11,30 @@ from game.common.map import Map
 from game.common.contract import Contract
 from game.common.truck import Truck
 
+from game.common.enums import NodeType, ActionType
+
 
 class buyController(Controller):
 
     def __init__(self):
         super().__init__()
     
-    # Generate list of contracts, store for verification
-    def buy_gas(self, client, percentGas = 1):
-        if(client.truck.current_node.node_type is )
-
-
-
-    def give_money(self, client, money):
-        client.truck.money += money
+    # control gas buying. Either buys the total amount of gas or the max amount allowable
+    def buy_gas(self, client):
+        gasPrice = random.uniform(1, 5) # gas price per percent
+        if(client.truck.current_node.node_type is NodeType.city and client.truck.money > 0):
+            percentRemain = 1 - client.truck.gas
+            maxPercent =  ( client.truck.money / gasPrice ) / 100
+            if(percentRemain < maxPercent):
+                client.truck.money -= percentRemain * gasPrice
+                client.truck.gas = 1
+            else:
+                client.truck.money = 0
+                client.truck.money += maxPercent
 
     # If contract was selected retrieve by index and store in Player, then clear the list
     def handle_actions(self, client):
-        if client.action._chosen_action is ActionType.select_contract:
-            client.active_contract = self.contract_list[int(client.action.contract_index)]
-            self.contract_list.clear()
+        if client.action._chosen_action is ActionType.buy_gas:
+            buy_gas(self, client)
 
 
