89d9ac047b3284ff828212788132cb270e86d3d8
qstanley <qastanley25@gmail.com>
2020-10-17 12:35:25 -0500

    Added RoadType and DisasterType to enums

--- game/common/stats.py
+++ game/common/stats.py
@@ -1,5 +1,6 @@
 from game.common.enums import Region
-
+from game.common.enums import RoadType
+from game.common.enums import DisasterType
 
 class GameStats:
     default_road_length = 100
@@ -9,7 +10,7 @@ class GameStats:
         Region.nord_dakotia: .6,
         Region.mobave_desert: .7,
         Region.mount_vroom: .8,
-        Region.loblantis: .8,
+        Region.lobslantis: .8,
         Region.tropical_cop_land: .9,
     }
 
@@ -18,6 +19,17 @@ class GameStats:
         Region.nord_dakotia: .6,
         Region.mobave_desert: .7,
         Region.mount_vroom: .8,
-        Region.loblantis: .8,
+        Region.lobslantis: .8,
         Region.tropical_cop_land: .9,
     }
+
+    road_length_modifier = {
+        RoadType.mountain_road: 1,
+        RoadType.forest_road: 1,
+        RoadType.tundra_road: 1.5,
+        RoadType.city_road: 1.5,
+        RoadType.highway: 2,
+        RoadType.interstate: 2
+    }
+
+
