1f6a54e6f005833e9747b1448a37e36ba27009ae
PixPanz <fjelstad1678@gmail.com>
2020-11-24 16:44:30 -0600

    nearly to the pr state

--- game/common/game_map.py
+++ game/common/game_map.py
@@ -5,9 +5,27 @@ from game.common.node import Node
 class GameMap(GameObject):
 
     # This will be a singly-linked list
-    # Sort-of. Kinda.
+    # Sort-of. Kinda. Python is weird about it
     def __init__(self, node=None):
         super().__init__()
         self.object_type = ObjectType.game_map
         self.head = node
 
+    def insert_node(self, node):
+        node.next_node = self.head
+        self.head = node
+    
+    def length(self):
+        i = 1
+        curr = self.head
+        if curr == None: return 0
+        while curr.next_node != None:
+            curr = curr.next_node
+            i += 1
+        return i
+
+    def to_json(self):
+        return
+    
+    def from_json(self):
+        return
