b054382525acc2750da11087882cadc222029770
Sophie Noel <54605853+Sopapia@users.noreply.github.com>
2021-02-06 14:51:48 -0600

    Visualizer updates - Contract and road
    Now displays large JSON values for contract. Will be changed once JSON changes are implemented. Road added

--- game/visualizer/json_script.gd
+++ game/visualizer/json_script.gd
@@ -14,13 +14,29 @@ func _ready():
 	if result_json.error == OK:
 		data = result_json.result
 	
-	$Label.text = str(data.get("1").get("time"))
+	
+	
+	var viewportWidth = get_viewport().size.x
+	var viewportHeight = get_viewport().size.y
+
+	var scale = viewportWidth / $TruckHUD.texture.get_size().x
+	
+
+	# Optional: Center the sprite, required only if the sprite's Offset>Centered checkbox is set
+	$TruckHUD.set_position(Vector2(viewportWidth/2, viewportHeight/2))
+
+	# Set same scale value horizontally/vertically to maintain aspect ratio
+	# If however you don't want to maintain aspect ratio, simply set different
+	# scale along x and y
+	$TruckHUD.set_scale(Vector2(scale, scale))
 	
 	$Timer.set_wait_time(1)
 	$Timer.start()
+	
+	
 
 
 func _on_Timer_timeout():
-	$Timer.set_wait_time(1)
-	$Label.text = str(data.get(str(turn)))
+	$lblContract.text = str(data.get(str(turn)).get("truck").get("contract_list"))
 	turn += 1
+
