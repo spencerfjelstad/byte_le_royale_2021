1db431e4475ec81a111d177a64101a7dd82d51a3
Sophie Noel <54605853+Sopapia@users.noreply.github.com>
2021-02-06 16:15:59 -0600

    Base Upgrade Spacing
    This doesn't work with the JSON so far, but establishes where things should go and that everything can fit.

--- game/visualizer/json_script.gd
+++ game/visualizer/json_script.gd
@@ -4,6 +4,10 @@ var turn = 1
 var turns = []
 var data = {}
 
+var body_sprites = ["res://assets/upgrades/upgrades_body_headlights.png","res://assets/upgrades/upgrades_body_sentry.png","res://assets/upgrades/upgrades_body_tank.png"]
+var tires_sprites = ["res://assets/upgrades/upgrades_tires_economy.png","res://assets/upgrades/upgrades_tires_monster.png","res://assets/upgrades/upgrades_tires_normal.png","res://assets/upgrades/upgrades_tires_sticky.png"]
+var addons_sprites = ["res://assets/upgrades/upgrades_addons_GPS.png","res://assets/upgrades/upgrades_addons_policescanner.png","res://assets/upgrades/upgrades_addons_rabbit_foot.png",]
+
 func _ready():
 	var file = File.new()
 	file.open("../../logs/turn_logs.json", file.READ)
@@ -20,6 +24,7 @@ func _ready():
 	var viewportHeight = get_viewport().size.y
 
 	var scale = viewportWidth / $TruckHUD.texture.get_size().x
+	
 	
 
 	# Optional: Center the sprite, required only if the sprite's Offset>Centered checkbox is set
@@ -38,5 +43,11 @@ func _ready():
 
 func _on_Timer_timeout():
 	$lblContract.text = str(data.get(str(turn)).get("truck").get("contract_list"))
+	
+	# Test if all upgrade textures fit and that I can change the sprite
+	$UpgBody.texture = load(body_sprites[turn%3])
+	$UpgTires.texture = load(tires_sprites[turn%4])
+	$UpgAddOns.texture = load(addons_sprites[turn%3])
+	
 	turn += 1
 
