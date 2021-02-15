760bb98fce4fd641cb2c6f0c831a792c9b28fcda
Sean Hagen <sean.r.hagen@gmail.com>
2021-02-15 17:24:10 -0600

    Updated client, added time subtraction for no actiontype chosen

--- base_client_sean.py
+++ base_client_sean.py
@@ -6,7 +6,10 @@ class Client(UserClient):
     # Variables and info you want to save between turns go here
     def __init__(self):
         super().__init__()
-        self.prevAction = -1
+        self.turn = 0
+        self.queue = []
+        self.low = 1000
+        self.high = 2000
 
     def team_name(self):
         """
@@ -14,6 +17,9 @@ class Client(UserClient):
         :return: Your team name
         """
         return 'Team Name'
+
+    def getMPG(self, speed):
+        return (-0.002649444*(speed**2))+(.2520296*speed)+.22752
 
     def calculateLength(self,map):
         currNode = map.head
@@ -28,7 +34,7 @@ class Client(UserClient):
 
     def canIMakeIt(self, truck):
         dist = self.chooseBestRoad(truck.map.current_node.roads)
-        return True if (((dist.length / 2) / (truck.body.max_gas * 100))*(1.05)) > 0 else False
+        return True if (dist.length/self.getMPG(truck.speed))/(truck.body.max_gas*100) - (dist.length * 1.1) > 0 else False
 
     def handleUpgrades(self, truck, bodyEnum, addonEnum):
         if truck.body.level < truck.addons.level:
@@ -54,6 +60,18 @@ class Client(UserClient):
         #print(bestIndex)
         return bestIndex
 
+    def chooseBestContract2(self, truck, contractList):
+        bestIndex = -1
+        for index in range(len(contractList)):
+            if self.turn < self.low and contractList[index]['contract'].difficulty == ContractDifficulty.easy:
+                bestIndex = index
+            elif self.turn < self.high and contractList[index]['contract'].difficulty == ContractDifficulty.medium:
+                bestIndex = index
+            elif self.turn > self.high and contractList[index]['contract'].difficulty == ContractDifficulty.hard:
+                bestIndex = index
+        #print(contractList[bestIndex]['contract'].difficulty == ContractDifficulty.easy)
+        return bestIndex
+
     def chooseBestRoad(self,roads):
         shortest = 10000
         index = -1
@@ -71,38 +89,34 @@ class Client(UserClient):
         :param actions:     This is the actions object that you will add effort allocations or decrees to.
         :param world:       Generic world information
         """
-        
-        if(truck.active_contract is None and ActionType.select_contract != self.prevAction):
+        self.turn += 1
+        if(truck.active_contract is None and ActionType.select_contract not in self.queue):
             # Select contract
-            ind = self.chooseBestContract(truck, truck.contract_list)
-            actions.set_action(ActionType.select_contract, 0)
-            self.prevAction = ActionType.select_contract
-            #print("Selecting index " + str(ind))
-        elif truck.speed != 47 and self.prevAction != ActionType.set_speed:
-            actions.set_action(ActionType.set_speed, 47)
-            self.prevAction = ActionType.set_speed
-            #print("Choose speed: " + str(truck.speed))
-        elif ActionType.buy_gas != self.prevAction and truck.body.current_gas < .15 and (truck.map.current_node.gas_price < truck.map.current_node.next_node.gas_price or self.canIMakeIt(truck)):
-            # Buy gas
-            #print("Gas")
-            actions.set_action(ActionType.buy_gas)
-            self.prevAction = ActionType.buy_gas
-        elif ActionType.repair != self.prevAction and ((truck.health < 70 and truck.map.current_node.repair_price < truck.map.current_node.next_node.repair_price * 1.5) or truck.health < 65):
+            ind = self.chooseBestContract2(truck, truck.contract_list)
+            actions.set_action(ActionType.select_contract, ind)
+            print("Selecting index " + str(ind))
+        elif ActionType.set_speed not in self.queue and (truck.speed != 47 and self.turn < self.high) or (truck.speed != 67 and self.turn > self.high):
+            actions.set_action(ActionType.set_speed, 47) if self.turn < self.high else actions.set_action(ActionType.set_speed, 67)
+            print('speed')
+        elif ActionType.buy_gas not in self.queue and (( truck.map.current_node.next_node is not None and truck.map.current_node.gas_price > truck.map.current_node.next_node.gas_price and self.canIMakeIt(truck)) or not self.canIMakeIt(truck)):
+                # Buy gas
+                #print("Gas")
+                actions.set_action(ActionType.buy_gas)
+        elif ActionType.repair != self.queue[0] and truck.health < 75:
             actions.set_action(ActionType.repair)
             #print("Heal")
-            self.prevAction = ActionType.repair
-        elif truck.tires != TireType.tire_econ and ActionType.upgrade != self.prevAction and truck.money > 1000:
-            actions.set_action(ObjectType.tires, TireType.tire_econ)
-            self.prevAction = ActionType.upgrade
-        elif  (truck.body.level < 3 and (10000 * 1.2 * (truck.body.level + 1)) < truck.money) and ActionType.upgrade != self.prevAction:
+        elif ActionType.upgrade not in self.queue and truck.money > 1000 and (truck.tires != TireType.tire_normal and self.turn < self.high) or (truck.tires != TireType.monster_truck and self.turn > self.high):
+            actions.set_action(ObjectType.tires, TireType.tire_normal) if self.turn < self.high else actions.set_action(ObjectType.tires, TireType.monster_truck)
+        elif  (truck.body.level < 3 and (10000 * 1.2 * (truck.body.level + 1)) < truck.money) and ActionType.upgrade not in self.queue:
             #print("upgrade")
-            actions.set_action(ActionType.upgrade, self.handleUpgrades(truck, ObjectType.tank, ObjectType.policeScanner))
-            self.prevAction = ActionType.upgrade
+            actions.set_action(ActionType.upgrade, self.handleUpgrades(truck, ObjectType.tank, ObjectType.GPS))
         else:
             # Move to next node
-            #print('move')
+            #print("move")
             actions.set_action(ActionType.select_route, self.chooseBestRoad(truck.map.current_node.roads))
-            self.prevAction = ActionType.select_route
+        if len(self.queue) > 1:
+            self.queue.pop(-1)
+        self.queue.insert(0, actions._chosen_action)
              
         pass
 
