629aa0c8863fc699ceb309e6d142c7421a0d3a1c
Sophie Noel <54605853+Sopapia@users.noreply.github.com>
2021-02-16 00:50:51 -0600

    Upgrades now synced with JSON

--- visualizer/json_script.gd
+++ visualizer/json_script.gd
@@ -37,22 +37,7 @@ var eventsDict = {
 	"rock": false
 }
 
-var body_sprites = ["res://assets/upgrades/upgrades_body_headlights.png","res://assets/upgrades/upgrades_body_sentry.png","res://assets/upgrades/upgrades_body_tank.png"]
-var tires_sprites = ["res://assets/upgrades/upgrades_tires_economy.png","res://assets/upgrades/upgrades_tires_monster.png","res://assets/upgrades/upgrades_tires_normal.png","res://assets/upgrades/upgrades_tires_sticky.png"]
-var addons_sprites = ["res://assets/upgrades/upgrades_addons_GPS.png","res://assets/upgrades/upgrades_addons_policescanner.png","res://assets/upgrades/upgrades_addons_rabbit_foot.png",]
-
 func _ready():
-	var file = File.new()
-	file.open("../logs/turn_logs.json", file.READ)
-	var text = file.get_as_text()
-	var result_json = JSON.parse(text)
-	file.close()
-	
-	if result_json.error == OK:
-		data = result_json.result
-	
-	
-	
 	var viewportWidth = get_viewport().size.x
 	var viewportHeight = get_viewport().size.y
 
@@ -77,6 +62,15 @@ func _input(event):
 		
 
 func _on_Timer_timeout():
+	var file = File.new()
+	file.open("../logs/turn_" + ("%04d" % turn) + ".json", file.READ)
+	var text = file.get_as_text()
+	var result_json = JSON.parse(text)
+	file.close()
+	
+	if result_json.error == OK:
+		data = result_json.result
+	
 	if(restart): 
 		OS.delay_msec(1000)
 		$RestartScreen.visible = false
@@ -85,45 +79,37 @@ func _on_Timer_timeout():
 	
 	
 	# Handle labels
-	var name = str(data.get(str(turn)).get("truck").get("active_contract").get("name")) + "\n"
-	var money_reward = "Payment: " + str(data.get(str(turn)).get("truck").get("active_contract").get("money_reward")) + "\n"
-	var renown_reward = "Renown: " + str(data.get(str(turn)).get("truck").get("active_contract").get("renown_reward")) + "\n"
-	var difficulty = "Difficulty: " + str(data.get(str(turn)).get("truck").get("active_contract").get("difficulty")) + "\n"
+	var name = str(data.get("truck").get("active_contract").get("name")) + "\n"
+	var money_reward = "Payment: " + str(data.get("truck").get("active_contract").get("money_reward")) + "\n"
+	var renown_reward = "Renown: " + str(data.get("truck").get("active_contract").get("renown_reward")) + "\n"
+	var difficulty = "Difficulty: " + str(data.get("truck").get("active_contract").get("difficulty")) + "\n"
 	
 	$lblContract.text = name + money_reward + renown_reward + difficulty
 	
-	$lblSpeed.text = str(data.get(str(turn)).get("truck").get("speed")) + " MPH"
-	$lblFuel.text = str(stepify(data.get(str(turn)).get("truck").get("body").get("current_gas"), 0.01) * 100) + "%"
-	$lblHealth.text = str(stepify(data.get(str(turn)).get("truck").get("health"), 0.01))
-	$lblTime.text = "Turn: " + str(turn)
-	$lblMoney.text = str(data.get(str(turn)).get("truck").get("money"))
-	$lblRenown.text = str(data.get(str(turn)).get("truck").get("renown"))
+	$lblSpeed.text = str(data.get("truck").get("speed")) + " MPH"
+	$lblFuel.text = str(stepify(data.get("truck").get("body").get("current_gas"), 0.01) * 100) + "%"
+	$lblHealth.text = str(stepify(data.get("truck").get("health"), 0.01))
+	$lblTime.text = str(data.get("time"))
+	$lblMoney.text = str(data.get("truck").get("money"))
+	$lblRenown.text = str(data.get("truck").get("renown"))
 	
-	var road_type = data.get(str(turn)).get("selected_route")
+	var road_type = data.get("selected_route")
 	change_road(road_type)
 	
-	var event_type = data.get(str(turn)).get("event")
+	var event_type = data.get("event")
 	show_event(event_type)
 	
-	#var current_city_name = str(data.get(str(turn)).get("truck").get("current_node").get(""))
 	if(road_type != 0):
 		spawn_sign("Plankton")
+		# vv Line to use when Chris implements shortened JSON
+		#spawn_sign(str(data.get("truck").get("current_node").get("city_name")))
 	
-	# Test if all upgrade textures fit and that I can change the sprite
-	$UpgBody.texture = load(body_sprites[turn%3])
-	$UpgTires.texture = load(tires_sprites[turn%4])
-	$UpgAddOns.texture = load(addons_sprites[turn%3])
+	show_upgrades_body(data.get("truck").get("body").get("object_type"))
+	show_upgrades_addons(data.get("truck").get("addons").get("object_type"))
+	show_upgrades_tires(data.get("truck").get("tires"))
 	
-	# Show Events
-	#if(turn % 3 == 0): spawn_bandits()
-	
-	# Variable for city sign
-	
-	# TruckHUD moves up and down like a truck bouncing on the road. Keep or no?
-	#if(turn % 2 == 0):
-	#	$TruckHUD.position.y += 2
-	#else:
-	#	$TruckHUD.position.y -= 2
+	$lblBody.text = "Body\nLvl " + str(data.get("truck").get("body").get("level"))
+	$lblAddOns.text =  "Add Ons\nLvl " + str(data.get("truck").get("addons").get("level"))
 	
 	turn += 1
 
@@ -187,7 +173,7 @@ func spawn_rock_slide():
 func spawn_traffic():
 	var traffic_instance = Traffic.instance()
 	add_child(traffic_instance)
-	move_child(traffic_instance, 1)
+	move_child(traffic_instance, 2)
 	pass
 
 func spawn_bandits():
@@ -238,3 +224,55 @@ func show_event(event_type):
 			spawn_police()
 		elif(event_type == 6):
 			spawn_traffic()
+
+func show_upgrades_body(upgrade_type):
+	var texture = ""
+	# None
+	if(upgrade_type == 0):
+		texture = "res://assets/Upgrades/upgrades_none.png"
+	# Tank	
+	elif(upgrade_type == 9):
+		texture = "res://assets/upgrades/upgrades_body_tank.png"
+	# Headlights
+	elif(upgrade_type == 11):
+		texture = "res://assets/upgrades/upgrades_body_headlights.png"
+	# Sentry Gun
+	elif(upgrade_type == 12):
+		texture = "res://assets/upgrades/upgrades_body_sentry.png"
+		
+	$UpgBody.texture = load(texture)	
+	
+func show_upgrades_addons(upgrade_type):
+	var texture = ""
+	# None
+	if(upgrade_type == 0):
+		texture = "res://assets/Upgrades/upgrades_none.png"
+	# Police Scanner	
+	elif(upgrade_type == 8):
+		texture = "res://assets/upgrades/upgrades_addons_policescanner.png"
+	# Rabbit Foot
+	elif(upgrade_type == 13):
+		texture = "res://assets/upgrades/upgrades_addons_rabbit_foot.png"
+	# GPS
+	elif(upgrade_type == 14):
+		texture = "res://assets/upgrades/upgrades_addons_GPS.png"
+		
+	$UpgAddOns.texture = load(texture)	
+
+func show_upgrades_tires(upgrade_type):
+	var texture = ""
+	# Normal
+	if(upgrade_type == 0):
+		texture = "res://assets/upgrades/upgrades_tires_normal.png"
+	# Economy	
+	elif(upgrade_type == 1):
+		texture = "res://assets/upgrades/upgrades_tires_economy.png"
+	# Sticky
+	elif(upgrade_type == 2):
+		texture = "res://assets/upgrades/upgrades_tires_sticky.png"
+	# Monster Truck
+	elif(upgrade_type == 3):
+		texture = "res://assets/upgrades/upgrades_tires_monster.png"
+	
+	$UpgTires.texture = load(texture)	
+	
