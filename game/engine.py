86c11a2e8f4fb6cd54b97721651482b49f70e4cf
Wyly Andrews <sirdude103@gmail.com>
2021-02-18 17:27:42 -0600

    minor error message change, clearing up confusion

--- game/engine.py
+++ game/engine.py
@@ -123,7 +123,7 @@ class Engine:
                                                 MAX_CLIENTS_START)
 
         if client_num_correct is not None:
-            self.shutdown(source='Client_error')
+            self.shutdown(source='Invalid number of clients found.')
 
         # Finally, request master controller to establish clients with basic objects
         if SET_NUMBER_OF_CLIENTS_START == 1:
@@ -226,7 +226,7 @@ class Engine:
                                                 MIN_CLIENTS_CONTINUE,
                                                 MAX_CLIENTS_CONTINUE)
         if client_num_correct is not None:
-            self.shutdown(source='Client_error')
+            self.shutdown(source='Invalid number of clients found.')
 
         # Finally, consult master controller for game logic
         if SET_NUMBER_OF_CLIENTS_START == 1:
