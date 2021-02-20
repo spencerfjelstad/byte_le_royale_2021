893d4328ea290cc669176e489a07cf97be80b797
Sean Hagen <sean.r.hagen@gmail.com>
2021-02-20 09:30:24 -0600

    Minor changes to base client

--- base_client.py
+++ base_client.py
@@ -6,6 +6,7 @@ class Client(UserClient):
     # Variables and info you want to save between turns go here
     def __init__(self):
         super().__init__()
+        self.turn = 0
 
     def team_name(self):
         """
@@ -22,30 +23,35 @@ class Client(UserClient):
         :param actions:     This is the actions object that you will add effort allocations or decrees to.
         :param world:       Generic world information
         """
-        
+        self.turn += 1
+
         chosen_upgrade = self.select_upgrade(actions, truck)
         # If there is not an active contract get one
         if(truck.active_contract is None):
-            print("Select")
+            #print("Select")
             chosen_contract = self.select_new_contract(actions, truck)
             actions.set_action(ActionType.select_contract, chosen_contract)
         # Buy gas if below 20% and there is enough money to fill tank to full at max gas price
         elif(truck.body.current_gas < .20 and truck.money > 100*truck.active_contract.game_map.current_node.gas_price):
-            print("Gas")
+            #print("Gas")
             actions.set_action(ActionType.buy_gas)
         # If health is below max and have enough money to fully repair do so
         elif truck.health < 100 and truck.money > 1000:
-            print("Heal")
+            #print("Heal")
             actions.set_action(ActionType.repair)
         elif chosen_upgrade is not None:
-            print("Upgrade")
+            #print("Upgrade")
             actions.set_action(ActionType.upgrade, chosen_upgrade)
         elif(truck.active_contract.game_map.current_node.next_node is not None):
             # Move to next node
             # Road can be selected by passing the index or road object
-            print("Move")
+            #print("Move")
             road = self.select_new_route(actions, truck)
             actions.set_action(ActionType.select_route, road)
+
+        if self.turn == 69 or self.turn == 420:
+            print("Funny Number!")
+        
         pass
 
     # These methods are not necessary, so feel free to modify or replace
