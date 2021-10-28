ccc4d29411f3bfb292d1e97a1c881d572356b127
Sean Hagen <49661807+HagenSR@users.noreply.github.com>
2021-10-27 23:55:51 -0500

    changed db to accept logs. Need to change so all clients are succesfully run against eachother or uncommit. Also run clients against the same seeds

--- server/client_runner.py
+++ server/client_runner.py
@@ -7,6 +7,7 @@ import zipfile
 from psycopg2.extras import RealDictCursor
 from joblib import Parallel, delayed
 import asyncio
+import random
 
 class client_runner:
 
@@ -30,26 +31,28 @@ class client_runner:
     def external_runner(self):
         clients = self.fetch_clients()
         self.group_id = self.insert_new_group_run()
-        for i in range(5):
-            res = Parallel(n_jobs = 1, backend="threading")(map(delayed(self.internal_runner), clients))
+        
+        # run each client 5 times, then run them in paralell using their index as a unique identifier
+        clients = clients * 5
+        res = Parallel(n_jobs = 5, backend="threading")(map(delayed(self.internal_runner), clients, [i for i in range(len(clients))]))
 
 
-    def internal_runner(self, row):
+    def internal_runner(self, row, index):
         score = 0
         error = ""
+        print(index)
         try:
             # Run game
             #self.log(f'Running client: {client}')
-            team_id = row['team_id']
             if not os.path.exists(f'server/temp'):
                 os.mkdir(f'server/temp')
-            end_path = f'server/temp/{row["submission_id"]}'
+            end_path = f'server/temp/{index}'
             if not os.path.exists(end_path):
                 os.mkdir(end_path)
             
             shutil.copy('launcher.pyz', end_path)
 
-            with open(f'{end_path}/client_{row["team_id"]}.py', 'w') as f:
+            with open(f'{end_path}/client_{index}.py', 'w') as f:
                 f.write(row['file_text'])
 
             # Copy and run proper file
@@ -65,11 +68,15 @@ class client_runner:
                 stdout, stderr = p.communicate()
 
             results = dict()
+            seed = ""
             if os.path.exists(end_path + '/logs/results.json'):
                 with open(end_path + '/logs/results.json', 'r') as f:
                     results = json.load(f)
                 score = results['player']['truck']['renown'] 
 
+            if os.path.exists(end_path + '/logs/game_map.json'):
+                with open(end_path + '/logs/game_map.json', 'r') as f:
+                    seed = str(f.readlines())
             # Save best log files? doesn't seem necessary (yet)
 
             if 'Error' in results and results['Error'] is not None:
@@ -82,7 +89,7 @@ class client_runner:
             #self.current_running.insert(0, number)
             f.close()
         finally:
-            self.insert_run(row["submission_id"], score, self.group_id, error)
+            self.insert_run(row["submission_id"], score, self.group_id, error, seed)
 
     def fetch_clients(self):
         cur = self.conn.cursor(cursor_factory= RealDictCursor)
@@ -95,9 +102,9 @@ class client_runner:
         self.conn.commit()
         return cur.fetchall()[0]["insert_group_run"]
 
-    def insert_run(self, subid, score, groupid, error):
+    def insert_run(self, subid, score, groupid, error, seed):
         cur = self.conn.cursor()
-        cur.execute("CALL insert_run(%s,%s,%s, %s)", (subid, score, groupid, error))
+        cur.execute("CALL insert_run(%s,%s,%s, %s, %s)", (subid, score, groupid, error, seed))
         self.conn.commit()
 
     def close_server(self):
